1a37e3c790ede05cb4da3ac00d68a6ffe091729f
OlgaSavytska <o.savytska77@gmail.com>
2019-09-16 18:33:09 +0300

    Updates

---
+++ precache-manifest.ca45dec06b7d49e2cc6c8b56f9c01959.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b209a7b1eb2a48cfc734ac9e4c8e8985",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "fd3f4646fcfd9d68d540",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.0f54525f.chunk.css"
+  },
+  {
+    "revision": "207b179c03938e727416",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.8554a60e.chunk.js"
+  },
+  {
+    "revision": "fd3f4646fcfd9d68d540",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.98b6e427.chunk.js"
+  },
+  {
+    "revision": "db10479c1573f6125dd1",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime~main.0eafb310.js"
+  }
+]);
